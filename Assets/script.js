b33dc3b4770dd67a6e3cd58e8d94511db17898cb
Masterchef842 <robertwilliamlord55@gmail.com>
2023-04-18 21:30:42 -0500

    rendered page

--- Assets/script.js
+++ Assets/script.js
@@ -1,28 +1,91 @@
-searchBtn=document.querySelector('#searchBtn');
-cityInput=document.querySelector('#cityInput');
+let searchBtn=document.querySelector('#searchBtn');
+let cityInput=document.querySelector('#cityInput');
+let weatherDiv=document.querySelector('#weather')
+let currentDay=dayjs().format("ddd, MMM D")
+let currentDayWeather=document.querySelector('#weatherData');
+let daysLater=document.querySelectorAll(".daysLater")
+let pastSearchesDiv=document.querySelector("#pastSearches")
+let pastSearches
 
 function getCityWeather(event){
     event.preventDefault();
     city=cityInput.value;
-    let latitude, longitude=getCoordinates(city);
-    weatherData=getAPI(city,latitude,longitude)
+    getCoordinates(city);
+    renderSearch();
+    
 }
 function getCoordinates(city){
-    city="austin"
-    let url="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid=30938dd6fcd531961e9f7d4e28342bde"
-    let responseData;
+    let url="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=30938dd6fcd531961e9f7d4e28342bde"
+    let latitude, longitude;
     fetch(url)
     .then(function(response){
         if(!response.ok)
             console.log(response.status)
         else
-            console.log(response.json())
+            response.json().then(function (data){
+                latitude=data[0].lat;
+                longitude=data[0].lon;
+                getWeather(latitude,longitude)
+                
+            })
             
     })
-    console.log(responseData);
 
 }
-function getAPI(city, latitude, longitude){
+function getWeather(latitude, longitude){
+    let url="http://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=30938dd6fcd531961e9f7d4e28342bde&units=imperial"
+    fetch(url)
+    .then(function(response){
+        if(!response.ok)
+            console.log(response.status)
+        else
+            response.json().then(function(data){
+                console.log(data);
+                renderPage(data);
+
+            })
+    })
+
+
+}
+function renderPage(data){
+
+    //main weather div
+    currentDayWeather.children[0].textContent=data.city.name+","+ data.city.country;
+    currentDayWeather.children[1].textContent= currentDay;
+    currentDayWeather.children[2].children[0].textContent="temp: "+data.list[0].main.temp+"°F";
+    currentDayWeather.children[2].children[1].textContent="wind: "+data.list[0].wind.speed+"mph";
+    currentDayWeather.children[2].children[2].textContent="humidty: "+ data.list[0].main.humidity+"%";
+    weatherDiv.setAttribute("class","col");
+    let hourIndex=7
+    for(let i=0;i<daysLater.length;i++){
+        daysLater[i].parentElement.children[0].textContent=dayjs().add(i+1,"day").format('dddd')
+        daysLater[i].parentElement.children[1].textContent=dayjs().add(i+1,"day").format('MMM, D')
+        daysLater[i].children[0].textContent=data.list[hourIndex].main.temp+"°F";
+        daysLater[i].children[1].textContent=data.list[hourIndex].wind.speed+"mph";
+        daysLater[i].children[2].textContent=data.list[hourIndex].main.humidity+"%";
+        hourIndex+=8
+
+
+    }
+}
+function renderSearch(){
+    if (localStorage.getItem("searches")==null) {
+        pastSearches.push(cityInput.value)
+        localStorage.setItem("searches", JSON.stringify(pastSearches));
+      } else {
+        pastSearches = JSON.parse(localStorage.getItem("searches"));
+        pastSearches.push(cityInput.value);
+        localStorage.setItem("searches", JSON.stringify(pastSearches));
+      }
+      
+
+
+    for(let i=0;i<pastSearches.length;i++){
+        let newSearch=document.createElement("li")
+        newSearch.innerHTML=pastSearches[i]
+        pastSearchesDiv.appendChild(newSearch);
+    }
 
 }
 
